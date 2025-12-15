f4bc2fca0e81b5c1388dacfd14aa42c32258c355
Antonio Jenaro <antonio.jenaro@gmail.com>
2025-12-15 13:05:44 +0100

    fix header

--- resources/views/puzzle/cabecera.blade.php
+++ resources/views/puzzle/cabecera.blade.php
@@ -51,9 +51,8 @@
                     <div class="container pt-2 spacebt bg-custom">
                         <ul class="buttonsleft">
                             <li class="active"><a href="/" class="linkcab">PROFESIONALES</a></li>
-                            <!--li><a href="https://gruposdigestivosepd.es/" class="linkcab">GRUPOS DE TRABAJO</a></li-->
-                            <li><a href="https://www.saludigestivo.es/" class="linkcab">POBLACION</a></li>
                             <li><a href="https://gruposdigestivosepd.es/" class="linkcab">GRUPOS DE TRABAJO</a></li>
+                            <li><a href="https://www.saludigestivo.es/" class="linkcab">POBLACION</a></li>
                         </ul>
                     </div>
                     <div class="container">
